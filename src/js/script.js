9815478783df632ca0afd6c53a0f607ed474faa7
Andre kh <khrisostomov94@yandex.ru>
2022-04-28 13:02:55 +0300

    end

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.slider__wrapper').slick({
+    prevArrow: false,
+    nextArrow: false,
+    dots: true
+  });
+});
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),                             
+      closeElem = document.querySelector('.menu__close');
+
+      hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+      });
+
+      closeElem.addEventListener('click', () => {
+        menu.classList.remove('active');
+      });
